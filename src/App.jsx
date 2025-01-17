9cd0c5d9b3aa76733d22f189c6ad832dc9d5ad66
Olena Surilova <elen21surilova@gmail.com>
2025-01-17 20:10:32 +0100

    first-project: create separate files for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,6 @@
-import reactImage from './assets/react-core-concepts.png';
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcept/CoreConcept';
 import {CORE_CONCEPTS} from './data';
-
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-function genRandomUnt(max) {
-    return Math.floor(Math.random() * (max +  1));
-}
-
-function Header() {
-    const description = reactDescription[genRandomUnt(reactDescription.length-1)];
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-          {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreComponent({title, description, image}) {
-    return (
-        <>
-            <img src={image} alt={title}/>
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </>
-    )
-}
 
 function App() {
   return (
@@ -38,7 +10,7 @@ function App() {
           <section id="core-concepts">
             <h2>Core Concepts</h2>
               <ul>
-                  {CORE_CONCEPTS.map(item => <li><CoreComponent {...item} /></li>)}
+                  {CORE_CONCEPTS.map(item => <li><CoreConcept {...item} /></li>)}
               </ul>
           </section>
       </main>
