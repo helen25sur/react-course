52a9b7922ecb41e4a2dac8665ffcac956bd016ab
Olena Surilova <elen21surilova@gmail.com>
2026-09-05 23:58:12 +0200

    tic-tac-toe: add functionality of editing the player name

--- react-essentials-tic-tac-toe/src/components/Player.jsx
+++ react-essentials-tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [username, setUsername] = useState(name);
+
+  function handleEditClick() {
+    setIsEditing(prev => !prev);
+  }
+
+  function handleInputChange(evt) {
+    setUsername(evt.target.value);
+  }
+
+  const userName = isEditing ? <input type="text" value={username} onChange={handleInputChange} required /> : <span className="player-name">{username}</span>;
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {userName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   )
 }
